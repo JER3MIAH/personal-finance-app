3d729c24f4c57903e8b43e929434ba9a6e8e1250
JER3MIAH <jeremiahbrownie@gmail.com>
2025-01-03 11:12:23 +0100

    feat: implement transaction logic

--- lib/src/features/transactions/transactions_injection_container.dart
+++ lib/src/features/transactions/transactions_injection_container.dart
@@ -1,10 +1,18 @@
+import 'package:personal_finance_app/src/app_injection_container.dart';
+import 'package:personal_finance_app/src/features/transactions/logic/blocs/transaction_bloc/transaction_bloc.dart';
+import 'package:personal_finance_app/src/features/transactions/logic/services/transaction_local_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class TransactionsInjectionContainer {
   static Future<void> init() async {
     //* Services
-    // getIt.registerLazySingleton<ThemeLocalService>(
-    //   () => ThemeLocalService(prefs: getIt<SharedPreferences>()),
-    // );
+    getIt.registerLazySingleton<TransactionLocalService>(
+      () => TransactionLocalService(prefs: getIt<SharedPreferences>()),
+    );
 
     //* Blocs
+    getIt.registerLazySingleton<TransactionBloc>(
+      () => TransactionBloc(localService: getIt<TransactionLocalService>()),
+    );
   }
 }
