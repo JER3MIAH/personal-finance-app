c6222928c4d3cd96573a75e03116b797df54b8b2
JER3MIAH <jeremiahbrownie@gmail.com>
2025-01-14 22:39:51 +0100

    fix: remove duplicate scaffold for pages

--- lib/src/features/pots/presentation/screens/pot_screen.dart
+++ lib/src/features/pots/presentation/screens/pot_screen.dart
@@ -20,75 +20,73 @@ class PotScreen extends StatelessWidget {
     }
 
     return BlocBuilder<PotsBloc, PotsState>(
-        builder: (_, potState) {
-          final pots = potState.pots;
+      builder: (_, potState) {
+        final pots = potState.pots;
 
-          return AppColumn(
-            shouldScroll: false,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: AutoSizeText(
-                      'Pots',
-                      style: textPreset1,
-                      maxLines: 1,
-                    ),
+        return AppColumn(
+          shouldScroll: false,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: AutoSizeText(
+                    'Pots',
+                    style: textPreset1,
+                    maxLines: 1,
                   ),
-                  if (isMobile)
-                    AddButton(
-                      onTap: addPot,
-                    )
-                  else
-                    AppButton(
-                      title: '+ Add New Pot',
-                      color: appColors.grey900,
-                      onTap: addPot,
-                    ),
-                ],
-              ),
-              YBox(20),
-              if (pots.isEmpty)
-                EmptyStateContainer(title: 'You currently have no pots'),
-              Expanded(
-                child: ScrollConfiguration(
-                  behavior: NoThumbScrollBehavior(),
-                  child: isDesktop
-                      ? GridView.builder(
-                          itemCount: pots.length,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            crossAxisSpacing: spacing200,
-                            mainAxisSpacing: spacing200,
-                            childAspectRatio: 5.8 / 3,
-                          ),
-                          itemBuilder: (context, index) {
-                            final pot = pots[index];
-                            return PotContainer(
-                              pot: pot,
-                            );
-                          },
-                        )
-                      : ListView.builder(
-                          itemCount: pots.length,
-                          itemBuilder: (context, index) {
-                            final pot = pots[index];
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.only(bottom: spacing200),
-                              child: PotContainer(
-                                pot: pot,
-                              ),
-                            );
-                          },
-                        ),
                 ),
+                if (isMobile)
+                  AddButton(
+                    onTap: addPot,
+                  )
+                else
+                  AppButton(
+                    title: '+ Add New Pot',
+                    color: appColors.grey900,
+                    onTap: addPot,
+                  ),
+              ],
+            ),
+            YBox(20),
+            if (pots.isEmpty)
+              EmptyStateContainer(title: 'You currently have no pots'),
+            Expanded(
+              child: ScrollConfiguration(
+                behavior: NoThumbScrollBehavior(),
+                child: isDesktop
+                    ? GridView.builder(
+                        itemCount: pots.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: spacing200,
+                          mainAxisSpacing: spacing200,
+                          childAspectRatio: 5.8 / 3,
+                        ),
+                        itemBuilder: (context, index) {
+                          final pot = pots[index];
+                          return PotContainer(
+                            pot: pot,
+                          );
+                        },
+                      )
+                    : ListView.builder(
+                        itemCount: pots.length,
+                        itemBuilder: (context, index) {
+                          final pot = pots[index];
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: spacing200),
+                            child: PotContainer(
+                              pot: pot,
+                            ),
+                          );
+                        },
+                      ),
               ),
-            ],
-          );
-        },
-      );
+            ),
+          ],
+        );
+      },
+    );
   }
 }
