edcb97f9a6682b53fce3f43b7d20a67608955750
JER3MIAH <jeremiahbrownie@gmail.com>
2025-01-15 00:25:37 +0100

    feat: implement budget and pot overview container

--- lib/src/features/home/presentation/components/overview/budgets_overview_container.dart
+++ lib/src/features/home/presentation/components/overview/budgets_overview_container.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:personal_finance_app/src/features/budget/data/models/budget.dart';
+import 'package:personal_finance_app/src/features/budget/logic/blocs/blocs.dart';
+import 'package:personal_finance_app/src/features/budget/presentation/components/budget_summary_pie_chart.dart';
 import 'package:personal_finance_app/src/shared/shared.dart';
 
 import 'see_details.dart';
@@ -12,29 +16,88 @@ class BudgetsOverviewContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(spacing300),
-      decoration: BoxDecoration(
-        color: appColors.white,
-        borderRadius: BorderRadius.circular(spacing150),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocBuilder<BudgetBloc, BudgetState>(
+      builder: (_, state) {
+        final budgets = state.budgets;
+
+        return Container(
+          padding: EdgeInsets.all(spacing300),
+          decoration: BoxDecoration(
+            color: appColors.white,
+            borderRadius: BorderRadius.circular(spacing150),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Text(
-                'Budgets',
-                style: textPreset2,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Budgets',
+                    style: textPreset2,
+                  ),
+                  buildSeeDetails(
+                    onTap: onSeeDetails,
+                  ),
+                ],
               ),
-              buildSeeDetails(
-                onTap: onSeeDetails,
+              YBox(spacing300),
+              Row(
+                spacing: spacing200,
+                children: [
+                  BudgetSummaryPieChart(budgets: budgets),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: spacing100,
+                    children: List.generate(
+                      budgets.length > 4 ? 4 : budgets.length,
+                      (index) {
+                        return _buildBudgetItem(
+                          budget: budgets[index],
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildBudgetItem({
+    required Budget budget,
+  }) {
+    return Row(
+      spacing: spacing200,
+      children: [
+        Container(
+          width: 4,
+          height: 43,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: getColorFromTheme(budget.theme),
+            borderRadius: BorderRadius.circular(spacing100),
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              budget.category,
+              style: textPreset5.copyWith(color: appColors.grey500),
+            ),
+            Text(
+              budget.spent.toString(),
+              style: textPreset4Bold,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/src/features/home/presentation/components/overview/pots_overview_container.dart
+++ lib/src/features/home/presentation/components/overview/pots_overview_container.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:personal_finance_app/src/features/pots/data/models/pot.dart';
+import 'package:personal_finance_app/src/features/pots/logic/blocs/pots_bloc/pots_bloc.dart';
+import 'package:personal_finance_app/src/features/pots/logic/blocs/pots_bloc/pots_state.dart';
 import 'package:personal_finance_app/src/shared/shared.dart';
 
 import 'see_details.dart';
@@ -12,29 +16,115 @@ class PotsOverviewContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(spacing300),
-      decoration: BoxDecoration(
-        color: appColors.white,
-        borderRadius: BorderRadius.circular(spacing150),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocBuilder<PotsBloc, PotsState>(
+      builder: (context, state) {
+        final pots = state.pots;
+
+        return Container(
+          padding: EdgeInsets.all(spacing300),
+          decoration: BoxDecoration(
+            color: appColors.white,
+            borderRadius: BorderRadius.circular(spacing150),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Text(
-                'Pots',
-                style: textPreset2,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Pots',
+                    style: textPreset2,
+                  ),
+                  buildSeeDetails(
+                    onTap: onSeeDetails,
+                  ),
+                ],
               ),
-              buildSeeDetails(
-                onTap: onSeeDetails,
+              YBox(spacing300),
+              Row(
+                spacing: spacing200,
+                children: [
+                  Container(
+                    width: 274,
+                    height: 117,
+                    padding: EdgeInsets.all(spacing300),
+                    decoration: BoxDecoration(
+                      color: appColors.beige100,
+                      borderRadius: BorderRadius.circular(spacing150),
+                    ),
+                    child: Row(
+                      spacing: spacing150,
+                      children: [
+                        SvgAsset(iconPot),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total Saved',
+                              style: textPreset4.copyWith(
+                                  color: appColors.grey500),
+                            ),
+                            Text(
+                              '\$${pots.fold(
+                                0.0,
+                                (prev, pot) => prev + pot.total,
+                              )}',
+                              style: textPreset1,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Wrap(
+                    spacing: spacing150,
+                    children: List.generate(
+                      pots.length > 4 ? 4 : pots.length,
+                      (index) => _buildPotItem(
+                        pot: pots[index],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildPotItem({
+    required Pot pot,
+  }) {
+    return Row(
+      spacing: spacing200,
+      children: [
+        Container(
+          width: 4,
+          height: 43,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: getColorFromTheme(pot.theme),
+            borderRadius: BorderRadius.circular(spacing100),
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              pot.name,
+              style: textPreset5.copyWith(color: appColors.grey500),
+            ),
+            Text(
+              pot.total.toString(),
+              style: textPreset4Bold,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
